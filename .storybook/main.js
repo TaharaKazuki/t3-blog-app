8b56b203b284b3580ef22d4f4cad1d71652e8a77
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-29 10:13:18 +0900

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
