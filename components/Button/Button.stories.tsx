ee1fa1f405c8db56b4983d09f391cb34a89f6e17
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-12-03 23:24:56 +0900

    update

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import { expect } from '@storybook/jest'
+import { screen, userEvent } from '@storybook/testing-library'
 
 import { Button } from './Button'
 
 export default {
   title: 'Controls/Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+export const PrimaryButton: ComponentStoryObj<typeof Button> = {
+  play: ({ args }) => {
+    userEvent.click(screen.getByRole('button'))
+    expect(args.onClick).toHaveBeenCalled()
+  },
+  args: {
+    color: 'primary',
+  },
+}
 
-export const BasicButton = Template.bind({})
+export const SecondaryButton: ComponentStoryObj<typeof Button> = {
+  ...PrimaryButton,
+  args: {
+    color: 'secondary',
+  },
+}
 
-BasicButton.args = {
-  children: 'Button',
+export const WarningButton: ComponentStoryObj<typeof Button> = {
+  ...PrimaryButton,
+  args: {
+    color: 'warning',
+  },
+}
+
+export const DangerButton: ComponentStoryObj<typeof Button> = {
+  ...PrimaryButton,
+  args: {
+    color: 'danger',
+  },
 }
